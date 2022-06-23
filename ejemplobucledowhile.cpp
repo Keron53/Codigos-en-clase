653d2f94b95a598a23aa2bcd1366dc6d7ce8a2e9
Keron <105449916+Keron53@users.noreply.github.com>
2022-06-23 11:25:32 -0500

    Update ejemplobucledowhile.cpp

---
+++ ejemplobucledowhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ int main()
+ {
+     cout<<endl<<"ejemplo del bucle do while";
+
+     int cont=10;
+     int tabla=5;
+     do{
+    cout<<endl<<tabla<<" X "<< cont <<"="<<(tabla*cont);
+    cont--;
+     }
+    while(cont>=1);
+    return 0;
+ }
