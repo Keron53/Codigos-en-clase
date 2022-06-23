04e1af8adf8f88ccee631eac15058cac8793a9ba
Keron <105449916+Keron53@users.noreply.github.com>
2022-06-23 11:27:03 -0500

    Update ejemplobuclewhile.cpp

---
+++ ejemplobuclewhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+cout<<"ejemplo del bucle while";
+int i=0;
+while (i<10)
+{
+i++;
+cout<<endl<<"mensaje # : "<< i;
+cout<<endl<<"el cuadrado de i: "<<(i*i);
+
+}
+return 0;
+}
